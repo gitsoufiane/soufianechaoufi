a80b7a365dd88eaeb984e79cf0b666dc0ee9676e
soufiane chaoufi <soufiane.chaoufi@gmail.com>
2023-11-06 10:23:46 -0800

    Add social media links to contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,27 @@
 import React from "react";
+import { FaGithub, FaLinkedinIn, FaX, FaInstagram, FaEnvelope } from "react-icons/fa6";
+import Link from "next/link";
 
 export default function page() {
-  return <div>Contact</div>;
+  return (
+    <div className="py-16 h-screen">
+      <div className="flex justify-evenly mt-10  items-center h-full">
+        <Link href="mailto:soufiane.chaoufi@gmail.com">
+          <FaEnvelope className="w-8 h-8" />
+        </Link>
+        <Link href={"https://github.com/gitsoufiane"}>
+          <FaGithub className="w-8 h-8" />
+        </Link>
+        <Link href={"https://www.linkedin.com/in/soufianechaoufi/"}>
+          <FaLinkedinIn className="w-8 h-8" />
+        </Link>
+        <Link href={""}>
+          <FaX className="w-8 h-8" />
+        </Link>
+        <Link href={"https://www.instagram.com/sfn880612"}>
+          <FaInstagram className="w-8 h-8" />
+        </Link>
+      </div>
+    </div>
+  );
 }
